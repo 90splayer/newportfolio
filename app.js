fb5bd3b6930b6f5e0cf6883a46b827f53781007c
Michael Ojogbo <linkupxc@gmail.com>
2022-07-17 02:57:09 +0100

    Second update

---
+++ app.js
@@ -0,0 +1,10 @@
+
+		var typed = new Typed("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about code & Design", "also teach programming to people.", "solve problems."
+			],
+			
+			typeSpeed: 150,
+            backspeed: 150,
+            loop: true
+        })
